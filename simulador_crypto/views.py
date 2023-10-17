ba52c660dbf7d29ab941794b3700b80a637d4454
Your Name01~clear <manudies@gmail.com>
2023-10-18 00:46:43 +0200

    feat: El navbar no funciona desde la página actual

--- simulador_crypto/views.py
+++ simulador_crypto/views.py
@@ -4,14 +4,17 @@ from . import app
 
 @app.route("/")
 def inicio():
-    return render_template('inicio.html')
+    ruta = "http://127.0.0.1:5000/"
+    return render_template('inicio.html', actual_inicio=ruta)
 
 
 @app.route("/purchase")
 def compra():
-    return render_template('compra.html')
+    ruta = "http://127.0.0.1:5000/purchase"
+    return render_template('compra.html', actual_compra=ruta)
 
 
 @app.route("/status")
 def estado():
-    return render_template('estado.html')
+    ruta = "http://127.0.0.1:5000/status"
+    return render_template('estado.html', actual_estado=ruta)
