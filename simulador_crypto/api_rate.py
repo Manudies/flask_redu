aa69248641c87ff2cd1776112642ccc2a433630d
Your Name01~clear <manudies@gmail.com>
2023-10-26 19:03:54 +0200

    feat modif consulta de cambio y comienzo consulta inversion

--- simulador_crypto/api_rate.py
+++ simulador_crypto/api_rate.py
@@ -18,15 +18,35 @@ def consultar_cambio(origen, destino):
     response = requests.request("GET", url, headers=headers, data=payload)
     json_response = response.json()
     if response.status_code == 200:
-        print(response.text)
-        time = json_response.get('time')
+        # print(response.text)
         rate = json_response.get('rate')
-        print(time, '-', rate)
-        return time, rate
+        return rate
     else:
         print('Algo no ha ido bien. Error ',
               response.status_code, response.reason)
 
 
 def consultar_inversion():
-    pass
+    # modificar según SQL invertido
+    endpoint = '/v1/assets?filter_asset_id=EUR;BTC;ETH;USDT;ADA;SOL;XRP;DOT;DOGE;SHIB'
+    url = server + endpoint
+    headers = {
+        'X-CoinAPI-Key': apikey
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        json_response = response.json()
+
+        for coin in json_response:
+            id = coin.get('asset_id')
+            # if (id in ['BTC', 'USD', 'EUR', 'ETH']):
+            #     print(id, '-', coin.get('name'), coin.get('type_is_crypto'))
+            print(id, '-', coin.get('name'), '-',
+                  coin.get('price_usd'), '-', coin.get('time'))
+    else:
+        print('Algo no ha ido bien. Error ',
+              response.status_code, response.reason)
+
+
+# consultar_cambio('EUR', 'USD')
