542094dd2948ccc774bee67ab41b478cf98bf04c
Your Name01~clear <manudies@gmail.com>
2023-10-26 18:57:06 +0200

    feat: implemento la página de compra

--- simulador_crypto/views.py
+++ simulador_crypto/views.py
@@ -1,6 +1,7 @@
+from .api_rate import consultar_cambio
+import datetime
 from .models import DBManager
-from flask import render_template
-import requests
+from flask import flash, redirect, render_template, request, url_for
 from . import app, RUTA
 from .forms import MovimientoForm
 
@@ -18,17 +19,35 @@ def inicio():
         archivo = ValueError
         if archivo:
             print("No he conseguido el try por que no hay base de datos")
-            sql_crear = 'CREATE TABLE "movimientos" ("id"	INTEGER NOT NULL UNIQUE,"fecha"	TEXT NOT NULL,"hora" TEXT NOT NULL,"moneda_from" TEXT NOT NULL,"cantidad_from" NUMERIC NOT NULL,"moneda_to" TEXT NOT NULL,"precio_unitario"	INTEGER NOT NULL,"cantidad_to" NUMERIC NOT NULL, PRIMARY KEY("id" AUTOINCREMENT))'
+            sql_crear = 'CREATE TABLE "movimientos" ("id"	INTEGER NOT NULL UNIQUE,"fecha"	TEXT NOT NULL,"hora" TEXT NOT NULL,"moneda_from" TEXT NOT NULL,"cantidad_from" NUMERIC NOT NULL,"moneda_to" TEXT NOT NULL,"cantidad_to" NUMERIC NOT NULL, PRIMARY KEY("id" AUTOINCREMENT))'
             db.crearSQL(sql_crear)
             movimientos = db.consultaSQL(sql_leer)
             print("Base de datos creada")
     return render_template('inicio.html', active_route='inicio', movs=movimientos)
 
 
-@app.route("/purchase", methods=['GET', 'POST'])
+@app.route('/purchase', methods=['GET', 'POST'])
 def compra():
-    formulario = MovimientoForm()
-    return render_template('compra.html', form=formulario, active_route='compra')
+    if request.method == 'GET':
+        formulario = MovimientoForm()
+        return render_template('compra.html', form=formulario, active_route='compra')
+    if request.method == 'POST':
+        formulario = MovimientoForm(data=request.form)
+        if formulario.calcular.data:
+            # Llamar a la API
+            formulario.precio_unitario.data = consultar_cambio(
+                formulario.moneda_from.data, formulario.moneda_to.data)
+            formulario.cantidad_to.data = float(
+                formulario.cantidad_from.data) * float(formulario.precio_unitario.data)
+            return render_template('compra.html', form=formulario, active_route='compra')
+        else:
+            # Guardo en la base de datos
+            print('Guardar en la BD')
+            db = DBManager(RUTA)
+            # consulta = f'INSERT INTO movimientos (fecha, hora, moneda_from, cantidad_from, moneda_to, cantidad_to) VALUES ({datetime("now")}, {datetime("localtime")}, {formulario.moneda_from.data}, {formulario.cantidad_from.data}, {formulario.moneda_to.data}, {formulario.cantidad_to.data})'
+            consulta2 = "INSERT INTO movimientos (fecha, hora, moneda_from, cantidad_from, moneda_to, cantidad_to) VALUES (date('now'), time('now'), 'EUR', 50, 'EUR', 50)"
+            db.crearEntrada(consulta2)
+        return render_template('compra.html', form=formulario, active_route='compra')
 
 
 @app.route("/status")
