519d70a67a80601fe8bbed84aff9935d88ab6bf1
Your Name01~clear <manudies@gmail.com>
2023-10-28 11:27:42 +0200

    fix: cambio tipos de campos del formulario

--- simulador_crypto/forms.py
+++ simulador_crypto/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField, HiddenField, SubmitField, SelectField, DateTimeLocalField
+from wtforms import DecimalField, HiddenField, SubmitField, SelectField, FloatField
 from wtforms.validators import DataRequired, NumberRange
 
 
@@ -11,17 +11,16 @@ class MovimientoForm(FlaskForm):
                               'EUR', 'BTC', 'ETH', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'DOGE', 'SHIB'],
                               validators=[DataRequired(message='No puede haber un movimiento sin cantidad')])
 
-    cantidad_from = DecimalField('Cantidad de origen', places=10,
-                                 validators=[DataRequired(message='No puede haber un movimiento sin cantidad'),
-                                             NumberRange(min=0.0000000001, message='no valido')])
+    cantidad_from = FloatField('Cantidad de origen',
+                               validators=[DataRequired(message='No puede haber un movimiento sin cantidad'),
+                                           NumberRange(min=0.0000000001, message='no valido')])
 
     moneda_to = SelectField('Moneda de destino', choices=[
         'EUR', 'BTC', 'ETH', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'DOGE', 'SHIB'],
         validators=[DataRequired(message='Eso no es un cambio')])
 
-    precio_unitario = DecimalField('Precio Unitario:', places=10, validators=[
-        NumberRange(min=0.0000000001, message='no valido')])
+    precio_unitario = FloatField('Precio Unitario:')
 
-    cantidad_to = DecimalField('Cantidad de destino:')
+    cantidad_to = FloatField('Cantidad de destino:')
     calcular = SubmitField('Calcular')
     validar = SubmitField('Validar')
