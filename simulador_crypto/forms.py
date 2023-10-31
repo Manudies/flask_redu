29e7e7a6de0ac6eb39c00056647a35f21753e3a8
Your Name01~clear <manudies@gmail.com>
2023-10-31 18:49:35 +0100

    change: monedas en formulario

--- simulador_crypto/forms.py
+++ simulador_crypto/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField, HiddenField, SubmitField, SelectField, FloatField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms import DecimalField, HiddenField, SubmitField, SelectField
+from wtforms.validators import DataRequired, ValidationError
 from wtforms_validators import NotEqualTo
 
 
 class MovimientoForm(FlaskForm):
+    monedas = ('EUR', 'ADA', 'BTC', 'DOGE', 'DOT', 'ETH',
+               'SHIB', 'SOL', 'USDT', 'XRP')
 
     def not_equal_to(self, campo):
         self.campo = campo
@@ -14,16 +16,14 @@ class MovimientoForm(FlaskForm):
     id = HiddenField()
     fecha = HiddenField()
     hora = HiddenField()
-    moneda_from = SelectField('Moneda de origen', choices=[
-                              'EUR', 'BTC', 'ETH', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'DOGE', 'SHIB'],
+    moneda_from = SelectField('Moneda de origen', choices=monedas,
                               validators=[DataRequired(message='Tienes que elegir una moneda')
                                           ])
 
     cantidad_from = DecimalField('Cantidad de origen',
                                  validators=[DataRequired(message='No puede haber un movimiento sin cantidad de origen')])
 
-    moneda_to = SelectField('Moneda de destino', choices=[
-                            'EUR', 'BTC', 'ETH', 'USDT', 'ADA', 'SOL', 'XRP', 'DOT', 'DOGE', 'SHIB'],
+    moneda_to = SelectField('Moneda de destino', choices=monedas,
                             validators=[DataRequired(message='No puede haber un movimiento sin cantidad'),
                                         NotEqualTo("moneda_from", message="Las monedas no pueden ser iguales")])
 
