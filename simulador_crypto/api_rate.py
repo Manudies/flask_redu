284bfc33d447be5014eb63b810ccaec0309c938f
Your Name01~clear <manudies@gmail.com>
2023-10-31 18:52:35 +0100

    feat: función consultar cambio todas las monedas

--- simulador_crypto/api_rate.py
+++ simulador_crypto/api_rate.py
@@ -6,9 +6,11 @@ load_dotenv()
 apikey = os.getenv('APIKEY')
 server = 'https://rest.coinapi.io'
 
+# Función de consulta de cambio entre dos monedas
+
 
 def consultar_cambio(origen, destino):
-    endpoint = f"/v1/exchangerate/{origen}/{destino}"
+    endpoint = f'/v1/exchangerate/{origen}/{destino}'
     url = server + endpoint
     payload = {}
     headers = {
@@ -18,7 +20,6 @@ def consultar_cambio(origen, destino):
     response = requests.request("GET", url, headers=headers, data=payload)
     json_response = response.json()
     if response.status_code == 200:
-        # print(response.text)
         rate = json_response.get('rate')
         return rate
     else:
@@ -26,24 +27,27 @@ def consultar_cambio(origen, destino):
               response.status_code, response.reason)
 
 
+# Consulta de monedas para estado de la inversión
 def consultar_inversion():
-    # modificar según SQL invertido
-    endpoint = '/v1/assets?filter_asset_id=EUR;BTC;ETH;USDT;ADA;SOL;XRP;DOT;DOGE;SHIB'
-    url = server + endpoint
+    endpoint = '/v1/exchangerate/EUR?filter_asset_id=EUR;BTC;ETH;USDT;ADA;SOL;XRP;DOT;DOGE;SHIB'
+    url = server+endpoint
+    payload = {}
     headers = {
+        'Accept': 'text/json',
         'X-CoinAPI-Key': apikey
     }
-    response = requests.get(url, headers=headers)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
     if response.status_code == 200:
         json_response = response.json()
+        rate = json_response["rates"]
+        indice = 0
+        rates = []
+        for i in rate:
+            rates.append(rate[indice]['rate'])
+            indice += 1
+        return rates
 
-        for coin in json_response:
-            id = coin.get('asset_id')
-            # if (id in ['BTC', 'USD', 'EUR', 'ETH']):
-            #     print(id, '-', coin.get('name'), coin.get('type_is_crypto'))
-            print(id, '-', coin.get('name'), '-',
-                  coin.get('price_usd'), '-', coin.get('time'))
     else:
         print('Algo no ha ido bien. Error ',
               response.status_code, response.reason)
